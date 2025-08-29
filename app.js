d0bccfe9bcefb6adfb594c46e301edd1f500e122
Sameer <176995632+SamPeterberg@users.noreply.github.com>
2025-08-29 21:30:54 +0500

    Refactor note input structure and enhance styling for improved usability

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-
-let add = document.getElementById("submit")
-let notesContainer = document.getElementById("notesContainer");
+var note = document.getElementById("noteInput");
 let notes = []
 
-function error(){
-  Swal.fire({
-      title: "❌ Alert!",
-      text: "Please write a Valid Note",
-      icon: "error",
-      confirmButtonText: "OK",
-    });
+
+
+
+  function addNote() {
+    if(note.value =="" || note.value ==" "){
+      error()
+    }else{
+      notes.push(note.value);
+      console.log(notes)
+      noteInput.value = "";
+      showNote()
+    }
+  }
+  
+  function showNote() {
+  var notesList = document.getElementById("notesList");
+  notesList.innerHTML = "  "
+  for (let i = 0; i < notes.length; i++) {
+      notesList.innerHTML = `<li id="note">
+                <span>${notes[i]}
+                </span>
+            </li>`;    
+  }
 }
 
-
-add.addEventListener("click", (e)=>{
-  e.preventDefault()
-  let noteInput = document.getElementById("note");
-  let note = noteInput.value;
-    if(note =="" || note ==" "){
-        error()
-    }else{
-      notes.push(note);
-      
-      noteInput.value = "";
+  function error(){
+    Swal.fire({
+        title: "❌ Alert!",
+        text: "Please write a Valid Note",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
     }
-})
-
